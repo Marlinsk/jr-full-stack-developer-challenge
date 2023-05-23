f38da1a5ce6fa5e7f6e2fe6f7cc3e2ce44cecd1e
Marlon <Marlonnlmx@outlook.com>
2023-05-23 11:16:16 -0300

    Cors implemented

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
